1db1f3ada5a7a019c9c27da829128831e8175773
hendisantika <hendisantika@yahoo.co.id>
2019-03-14 06:06:32 +0700

    #3 - Add Index Controller

---
+++ src/main/java/com/hendisantika/springbootdockersample/IndexController.java
@@ -0,0 +1,26 @@
+package com.hendisantika.springbootdockersample;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-docker-sample
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 2019-03-14
+ * Time: 06:05
+ */
+
+@RestController
+@RequestMapping("/")
+public class IndexController {
+    @GetMapping
+    String index() {
+        return "Hello Docker Wordl! " + new Date();
+    }
+}
