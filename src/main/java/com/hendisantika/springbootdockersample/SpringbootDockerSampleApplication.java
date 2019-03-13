c29c2b001431214841db3ae6efa5b004d9ce25cb
hendisantika <hendisantika@yahoo.co.id>
2019-03-14 06:05:14 +0700

    #1 - Initial Commit

---
+++ src/main/java/com/hendisantika/springbootdockersample/SpringbootDockerSampleApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootdockersample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDockerSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootDockerSampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hendisantika/springbootdockersample/SpringbootDockerSampleApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hendisantika.springbootdockersample;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootDockerSampleApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
